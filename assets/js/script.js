d65df4e38ed1002e827f7b0062988a0f7d1d443f
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-06-08 20:33:33 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var cityName ="";
+var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
+var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=springville&appid=803fa34dbd4909977dd765eb002a2987";
+
+// var currentWeather =function(user){
+//     fetch(currentWeatherAPI)
+        // .then(function(response) {
+        //   return response.json();
+        // })
+        // .then(function(data) {
+        //   console.log(data);
+        // });
+// }
+var currentWeather = function(location){
+    var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
+    fetch(currentWeatherAPI).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        })
+    });
+};
+
+var userLocation = function(){
+    console.log("function was called");
+};
+userLocation();
+currentWeather("springville");
