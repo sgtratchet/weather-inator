ab0aab8cba4d8cda5bba9f9423a12de28d454245
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-06-08 23:18:49 -0600

    started the specific data draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 var cityBtnEl =document.querySelector("#city-button");
-var userInputEl = document.querySelector("#user-input")
+var userInputEl = document.querySelector("#user-input");
 var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
 var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
+var weatherContainerEl = document.querySelector('#weather-container');
+var weatherSearchTerm = document.querySelector('#weather-search');
+
 
 var currentWeather = function(location){
     var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
     fetch(currentWeatherAPI).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            displayWeather(data, location)
+            // console.log(data);
         })
     });
 };
@@ -21,9 +25,22 @@ var formSubmitHandler = function(event){
     } else {
         alert('Please enter a city name')
     }
-    console.log(event);
+    // console.log(event);
 };
 
-// currentWeather("springville");
+var displayWeather = function(weather, inputCity){
+    weatherContainerEl.textContent ="";
+    weatherSearchTerm.textContent = inputCity;
+
+    var weatherTemp = weather.main.temp;
+    var weatherHumidity = weather.main.humidity;
+    var weatherWind = weather.wind.speed;
+    // var weatherIcon = weather.weather.0.icon;
+    console.log(weatherTemp);
+    console.log(weatherHumidity);
+    console.log(weather);
+    console.log(inputCity);
+}
+
 
 cityBtnEl.addEventListener("click",formSubmitHandler);
