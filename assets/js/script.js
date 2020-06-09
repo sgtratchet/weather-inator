b309296124e1c95c14cf425de37f72bac7c4f8b7
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-06-08 21:06:29 -0600

    added button, created fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var cityName ="";
 var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
-var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=springville&appid=803fa34dbd4909977dd765eb002a2987";
+var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
 
 var currentWeather = function(location){
     var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
