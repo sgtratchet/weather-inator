35c38f39b7b73d69bce45176290048ce15693b9b
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-06-14 23:54:03 -0600

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var cityBtnEl =document.querySelector("#city-button");
 var userInputEl = document.querySelector("#user-input");
-var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
-var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
-var weatherContainerEl = document.querySelector('#weather-container');
+var weatherContainerEl = document.getElementById('weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search');
-
+var displayWeatherSpan = document.getElementById('display-weather');
+var fiveDayContainer = document.getElementById('five-day-container');
+var recentSearchContainer = document.getElementById('recent-search');
+var recentButtonIndex = 0;
 
 var currentWeather = function(location){
     var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
@@ -15,48 +16,114 @@ var currentWeather = function(location){
         })
     });
 };
+var fiveDayForcast = function(location){
+    var fiveDayForecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=imperial&appid=803fa34dbd4909977dd765eb002a2987";
+    fetch(fiveDayForecastAPI).then(function(response){
+        response.json().then(function(data){
+            displayFiveDay(data, location)
+            // console.log(data);
+        })
+    });
+};
+
 var formSubmitHandler = function(event){
     event.preventDefault();
     var cityLocation = userInputEl.value.trim();
 
     if (cityLocation){
         currentWeather(cityLocation);
+        fiveDayForcast(cityLocation);
+        displayRecent(cityLocation);
         userInputEl.value = "";
+        recentButtonIndex++;
     } else {
         alert('Please enter a city name')
     }
     // console.log(event);
 };
 
+var recentButtonHandler = function(){
+    var recentLocation = event.target.getAttribute("location");
+    currentWeather(recentLocation);
+    fiveDayForcast(recentLocation);
+};
+
 var displayWeather = function(weather, inputCity){
     weatherContainerEl.textContent ="";
     weatherSearchTerm.textContent = inputCity;
+    weatherIcon = weather.weather[0].icon;
+    var weatherIconImg = document.createElement("img");
+    weatherIconImg.src = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+    weatherSearchTerm.appendChild(weatherIconImg);
+    
 
     var weatherTemp = weather.main.temp;
     var weatherHumidity = weather.main.humidity;
     var weatherWind = weather.wind.speed;
-    // var weatherIcon = weather.weather.0.icon;
-    console.log(weatherTemp);
-    console.log(weatherHumidity);
+    var weatherType = weather.weather.description;
+
     console.log(weather);
-    console.log(inputCity);
+    // console.log(inputCity);
 
-    var weatherEl = document.createElement('div');
-    weatherEl.classlist ="card";
+    var weatherContainerTemp = document.createElement('P');
+        weatherContainerTemp.innerHTML = "Temperature: " + weatherTemp;
+        weatherContainerEl.appendChild(weatherContainerTemp);
 
-    var cardBodyEl = document.createElement('div');
-    cardBodyEl.setAttribute("class", 'card-body');
+    var weatherContainerHumidity = document.createElement('P');
+        weatherContainerHumidity.innerHTML = "Humidity: " + weatherHumidity;
+        weatherContainerEl.appendChild(weatherContainerHumidity);
 
-    var weatherCardDate = document.createElement
-    var weatherCardIcon = document.createElement
-    var weatherCardTemp = document.createElement
-    var weatherCardHumidity = document.createElement
+    var weatherContainerWind = document.createElement('P');
+        weatherContainerWind.innerHTML = "Wind Speed: " + weatherWind;
+        weatherContainerEl.appendChild(weatherContainerWind);
 
-    weatherEl.appendChild(cardBodyEl);
-    weatherContainerEl.appendChild(weatherEl);
-    // var cardTitleEl = document.createElement('h5')
+};
+var displayFiveDay = function(fiveDay, inputCity){
+    fiveDayContainer.textContent = "";
+    inputCity.trim;
+    console.log(fiveDay);
+
+    for(var i=0; i<40;i=i+8){
+        var fiveDayCard = document.createElement("div");
+            fiveDayCard.setAttribute("class", "card justify-space-between col-sm");
+            // fiveDayCard.setAttribute("style", "width: 18rem;");
+            fiveDayContainer.appendChild(fiveDayCard);
+
+        var fiveDayDate = fiveDay.list[i].dt_txt;
+        
+        fiveDayIcon = fiveDay.list[i].weather[0].icon;
+        var fiveDayIconImg = document.createElement("img");
+        fiveDayIconImg.src = "http://openweathermap.org/img/wn/" + fiveDayIcon + "@2x.png";
+        fiveDayCard.appendChild(fiveDayIconImg);
+
+        var fiveDayTemp = fiveDay.list[i].main.temp;
+            fiveDayTemp = Math.round(fiveDayTemp);
+    
+        var fiveDayHumid = fiveDay.list[i].main.humidity;
+
+        var fiveDayInfoDate = document.createElement("P");
+            fiveDayInfoDate.innerHTML = fiveDayDate;
+            fiveDayCard.appendChild(fiveDayInfoDate);
+
+        var fiveDayInfoTemp = document.createElement("P");
+            fiveDayInfoTemp.innerHTML = "Temp: " + fiveDayTemp;
+            fiveDayCard.appendChild(fiveDayInfoTemp);
+
+        var fiveDayInfoHumid = document.createElement("P");
+            fiveDayInfoHumid.innerHTML = "Humidity: " + fiveDayHumid;
+            fiveDayCard.appendChild(fiveDayInfoHumid);
+    }
+}
+var displayRecent = function(location){
+
+    var recentSearch = document.createElement("button");
+        recentSearch.setAttribute('class', 'form-control btn btn-outline-secondary')
+        recentSearch.setAttribute('id', recentButtonIndex);
+        recentSearch.innerHTML = location;
+        recentSearch.setAttribute('location', location);
+        recentSearch.addEventListener("click", recentButtonHandler);
+        recentSearchContainer.appendChild(recentSearch);
+        
 
 }
-
-
 cityBtnEl.addEventListener("click",formSubmitHandler);
